cd8766e5379d56f0e771d3f5e69d1e22449a1f50
Scott G. Daniel <scottdaniel25@gmail.com>
2016-01-30 17:15:59 -0600

    more verbiage

--- setup_perlbrew.sh
+++ setup_perlbrew.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 CWD=$(pwd)
 cd $HOME
+read -p "This might take a while, <CTRL-C> to abort, otherwise press [Enter]"
 perlbrew install perl-5.22.1
 perlbrew switch perl-5.22.1
 perlbrew lib create default
 perlbrew switch perl-5.22.1@default
 perlbrew install-cpanm
+cpanm Bio::Index::Fastq
 echo Done
 cd $CWD
