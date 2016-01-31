b5bc355d7cf8e76aee8e9a2121f29d7241b252e2
Scott G. Daniel <scottdaniel25@gmail.com>
2016-01-30 22:48:35 -0600

    more perl modules added

--- setup_perlbrew.sh
+++ setup_perlbrew.sh
@@ -7,6 +7,12 @@ perlbrew switch perl-5.22.1
 perlbrew lib create default
 perlbrew switch perl-5.22.1@default
 perlbrew install-cpanm
-cpanm Bio::Index::Fastq
+perlbrew clean
+cpanm --prompt Module::Build
+cpanm --prompt Perl::Critic
+cpanm --prompt Perl::Tidy
+cpanm --prompt Bio::Perl
+cpanm --prompt Text::Autoformat
+cpanm --prompt common::sense
 echo Done
 cd $CWD
