6a96ce9a934491e2f48479dd939fa16934f95c68
Scott Daniel <scottdaniel25@gmail.com>
2016-02-18 20:13:32 -0700

    more fixes

--- setup_perlbrew.sh
+++ setup_perlbrew.sh
@@ -3,10 +3,11 @@ CWD=$(pwd)
 cd $HOME
 read -p "This might take a while, <CTRL-C> to abort, otherwise press [Enter]"
 perlbrew install perl-5.22.1
+sleep 3
 perlbrew switch perl-5.22.1
 perlbrew lib create default
 perlbrew switch perl-5.22.1@default
-perlbrew-cpanm
+perlbrew install-cpanm
 perlbrew clean
 cpanm --prompt Module::Build
 cpanm --prompt Perl::Critic
