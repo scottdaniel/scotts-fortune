dee162177d95b8500bbd7f6f48fef16f65cae28b
Scott G. Daniel <scottdaniel25@gmail.com>
2019-02-19 13:55:36 -0500

    better

--- after_conda_setup_runme.sh
+++ after_conda_setup_runme.sh
@@ -1,4 +1,4 @@
-echo Welcome to Scott\'s environment setup script!
+echo Welcome to Scott\'s conda setup script!
 
 export HOME=$HOME
 export INSTALL_DIR=$(pwd)
@@ -6,6 +6,7 @@ export INSTALL_DIR=$(pwd)
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
+conda config --add channels biobakery
 
-conda install fortune
+conda install -c asmeurer fortune
 conda install -c bioconda coreutils
